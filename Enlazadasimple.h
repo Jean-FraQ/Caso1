1c22d0e61ca945854fc2730f9f033876dbabb938
Jean-FraQ <jeanfqj24@estudiantec.cr>
2023-08-25 18:15:18 -0600

    Añadido header para la lista

---
+++ Enlazadasimple.h
@@ -0,0 +1,8 @@
+void agregar (info pdato){} //Agrega un elemento al final de la lista
+int largo (){} //Obtiene el número de elementos de la lista (o su largo)
+void agregarpos (info pdato, int posicion){} //Agrega un elemento en la posición seleccionada, si el mayor al largo de la lista lo inserta al final
+void borrarultimo (){} //Elimina el último elemento agregdo a la lista
+void borrarpos (int posicion){} //Elimina el elemento de la posiión solicitada
+void borraritem (info pdato){} //Elimina los elementos que contengan el item seleccionado
+void desplegar (int posicion){} //Imprime el elemento seleccionado 
+void displayall (){} //Imprime todos los elementos de la lista
